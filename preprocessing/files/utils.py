8cd9aca9ac431f580ccb8b94b08177d18114882e
Pimblott, Matthew (DLSLtd,RAL,LSCI) <matthew.pimblott@diamond.ac.uk>
2023-07-12 17:14:50 +0100

    fix first issues

--- preprocessing/files/utils.py
+++ preprocessing/files/utils.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from preprocessing.files.hdf5 import H5Handler
 from preprocessing.files.npy import NPYHandler
 from preprocessing.files.tif import TiffHandler
+from preprocessing.files.file_handler import FileHandler
 
-def get_handler(path: Path):
+def get_handler(path: Path, *args, **kwargs) -> FileHandler:
     for handler in [H5Handler, NPYHandler, TiffHandler]:
-        if handler.check_ext(path):
-            return handler
+        if handler().check_ext(path):
+            return handler(*args, **kwargs)
     raise ValueError(f'No handler found for {path}')
--- preprocessing/main.py
+++ preprocessing/main.py
@@ -8,27 +8,28 @@ import preprocessing.image_processing as ip
 
 @click.command()
 @click.argument('data_path', type=click.Path(exists=True))
-@click.argument('output_dir', type=click.Path(exists=True))
+@click.argument('output_dir', type=click.Path(exists=True), default=Path().absolute())
 @click.option('-sd', '--sd_factor', default=1.5, help='How many standard deviations to clip')
 @click.option('-ot', '--output-type', default='h5', help='Output file type, h5 or tiff')
 @click.option('-r', '--rescale', is_flag=True, help='Rescale the data to [0, 1]', default=True)
-def clip(data_path, output_path, sd_factor, output_type, rescale):
+def clip(data_path, output_dir, sd_factor, output_type, rescale):
     data_path = Path(data_path)
-    output_path = Path(output_path)
-    handle = get_handler(data_path)(file=data_path)
+    output_dir = Path(output_dir)
+    handle = get_handler(data_path, file=data_path)
     data = handle.read()
     lb, ub = clip_to_upper_hist(
         data.flatten(),
-        output_path,
+        output_dir,
         sd_factor=sd_factor,
     )
     data[data < lb] = np.NaN
     data[data > ub] = ub
-    print(f'min: {np.nanmin(data)}, max: {np.nanmin(data)}')
+    print(f'min: {np.nanmin(data)}, max: {np.nanmax(data)}')
+    print(f'rescale: {rescale}')
     if rescale:
         data = (data - np.nanmin(data)) / (np.nanmax(data) - np.nanmin(data))
         print(f'min: {np.nanmin(data)}, max: {np.nanmax(data)}')
-    out_path = output_path / f'{data_path.stem}_clipped.{output_type}'
+    out_path = output_dir / f'{data_path.stem}_clipped.{output_type}'
     output_handler = get_handler(out_path)
     output_handler.write(
         out_path,
@@ -40,17 +41,17 @@ def clip(data_path, output_path, sd_factor, output_type, rescale):
 @click.argument('data_path', type=click.Path(exists=True))
 @click.argument('output_dir', type=click.Path(exists=True), default=Path().absolute())
 @click.option('-s', '--sigma', default=1.5, help='Sigma value for Gaussian filter')
-@click.option('-ot', '--output-type', default='h5', help='Output file type, h5 or tiff')
-def gaussian_blur(data_path, output_path, sigma, output_type):
+@click.option('-t', '--output-type', default='h5', help='Output file type, h5 or tiff')
+def gaussian_blur(data_path, output_dir, sigma, output_type):
     data_path = Path(data_path)
-    output_path = Path(output_path)
+    output_dir = Path(output_dir)
     handle = get_handler(data_path)(file=data_path)
     data = ip.denoise(
         handle.read(),
-        output_path,
+        output_dir,
         sigma=sigma,
     )
-    out_path = output_path / f'{data_path.stem}_denoised.{output_type}'
+    out_path = output_dir / f'{data_path.stem}_denoised.{output_type}'
     output_handler = get_handler(out_path)
     output_handler.write(
         out_path,
